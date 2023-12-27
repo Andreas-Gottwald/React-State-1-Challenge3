6d6650fa5b164269199fa626936a24fa467a62a3
Andreas Gottwald <andreas.gottwald@hotmail.de>
2023-12-27 23:40:55 +0100

    finish

--- src/App.js
+++ src/App.js
@@ -1,52 +1,42 @@
 import React from "react";
 import "./styles.css";
+import { useState } from "react";
+
+const validCode = "🐡🐠🐋";
 
 export default function App() {
-  let code = "?";
+  const [code, setCode] = useState();
 
-  const validCode = "🐡🐠🐋";
+  function handleFishClick(event) {
+    const clickedFish = event.target.textContent;
+    setCode(code + clickedFish);
+    console.log(`Clicked fish: ${clickedFish}`);
+  }
 
+  function handleResetClick() {
+    setCode("");
+  }
   return (
     <div className="container">
       <div className="button-container">
-        <button
-          type="button"
-          onClick={() => {
-            console.log("Update Code!");
-          }}
-        >
+        <button type="button" onClick={handleFishClick}>
           <span role="img" aria-label="Pufferfish">
             🐡
           </span>
         </button>
-        <button
-          type="button"
-          onClick={() => {
-            console.log("Update Code!");
-          }}
-        >
+        <button type="button" onClick={handleFishClick}>
           <span role="img" aria-label="Whale">
             🐋
           </span>
         </button>
-        <button
-          type="button"
-          onClick={() => {
-            console.log("Update Code!");
-          }}
-        >
+        <button type="button" onClick={handleFishClick}>
           <span role="img" aria-label="Clownfish">
             🐠
           </span>
         </button>{" "}
       </div>
 
-      <button
-        type="button"
-        onClick={() => {
-          console.log("Reset Code!");
-        }}
-      >
+      <button type="button" onClick={handleResetClick}>
         Reset
       </button>
 
